a2650022f98149aeb65001877335cd7e520a116f
OhNooooooooo <109457389+OhNooooooooo@users.noreply.github.com>
2023-08-30 17:00:57 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+
+status = "";
+
+
+
+function setup(){
+    canvas = createCanvas(480,380);
+    canvas.center();
+
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    input = document.getElementById("input").innerText
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+}
